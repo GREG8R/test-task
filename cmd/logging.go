ffc5d0f8717dd0ee098e3b3d0a26955b56a50a7a
Grigoriy Kolotilin <gpkolotilin@avito.ru>
2021-03-19 22:20:20 +0300

    init proj

---
+++ cmd/logging.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"btcn_srv/pkg/services/bitcoin_service"
+	"time"
+
+	"github.com/go-kit/kit/log"
+
+	"github.com/GREG8R"
+)
+
+type loggingMiddleware struct {
+	logger log.Logger
+	next   bitcoin_service.StringService
+}
+
+func (mw loggingMiddleware) Uppercase(s string) (output string, err error) {
+	defer func(begin time.Time) {
+		_ = mw.logger.Log(
+			"method", "uppercase",
+			"input", s,
+			"output", output,
+			"err", err,
+			"took", time.Since(begin),
+		)
+	}(time.Now())
+
+	output, err = mw.next.Uppercase(s)
+	return
+}
+
+func (mw loggingMiddleware) Count(s string) (n int) {
+	defer func(begin time.Time) {
+		_ = mw.logger.Log(
+			"method", "count",
+			"input", s,
+			"n", n,
+			"took", time.Since(begin),
+		)
+	}(time.Now())
+
+	n = mw.next.Count(s)
+	return
+}
---
+++ cmd/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"btcn_srv/pkg/services/bitcoin_service"
+	"net/http"
+	"os"
+
+	stdprometheus "github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+
+	"github.com/go-kit/kit/log"
+	kitprometheus "github.com/go-kit/kit/metrics/prometheus"
+	httptransport "github.com/go-kit/kit/transport/http"
+
+	//"../pkg/services/bitcoin_service"
+)
+
+func main() {
+	logger := log.NewLogfmtLogger(os.Stderr)
+
+	fieldKeys := []string{"method", "error"}
+	requestCount := kitprometheus.NewCounterFrom(stdprometheus.CounterOpts{
+		Namespace: "my_group",
+		Subsystem: "string_service",
+		Name:      "request_count",
+		Help:      "Number of requests received.",
+	}, fieldKeys)
+	requestLatency := kitprometheus.NewSummaryFrom(stdprometheus.SummaryOpts{
+		Namespace: "my_group",
+		Subsystem: "string_service",
+		Name:      "request_latency_microseconds",
+		Help:      "Total duration of requests in microseconds.",
+	}, fieldKeys)
+	countResult := kitprometheus.NewSummaryFrom(stdprometheus.SummaryOpts{
+		Namespace: "my_group",
+		Subsystem: "string_service",
+		Name:      "count_result",
+		Help:      "The result of each count method.",
+	}, []string{}) // no fields here
+
+	var svc bitcoin_service.StringService
+	svc = bitcoin_service.SService{}
+	svc = loggingMiddleware{logger, svc}
+	svc = instrumentingMiddleware{requestCount, requestLatency, countResult, svc}
+
+	uppercaseHandler := httptransport.NewServer(
+		bitcoin_service.MakeUppercaseEndpoint(svc),
+		bitcoin_service.DecodeUppercaseRequest,
+		bitcoin_service.EncodeResponse,
+	)
+
+	countHandler := httptransport.NewServer(
+		bitcoin_service.MakeCountEndpoint(svc),
+		bitcoin_service.DecodeCountRequest,
+		bitcoin_service.EncodeResponse,
+	)
+
+	http.Handle("/uppercase", uppercaseHandler)
+	http.Handle("/count", countHandler)
+	http.Handle("/metrics", promhttp.Handler())
+	logger.Log("msg", "HTTP", "addr", ":8080")
+	logger.Log("err", http.ListenAndServe(":8080", nil))
+}
